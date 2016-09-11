30cd27099a89200718de0339124c545454f22993
funderburkjim <funderburkjim@gmail.com>
2016-09-11 18:19:58 -0400

    revise whitelist/redo.sh
    change path to hwnorm1c.txt

--- ejf/hwnorm1c/whitelist/redo.sh
+++ ejf/hwnorm1c/whitelist/redo.sh
@@ -5,6 +5,7 @@ cd auxiliary
 cat special_*.txt > special.txt
 cd ../
 echo "regenerating graylist.txt and all whitelistX.txt files"
-HWNORM1C=~/Documents/GitHub/hwnorm1/ejf/hwnorm1c/hwnorm1c.txt
+#HWNORM1C=~/Documents/GitHub/hwnorm1/ejf/hwnorm1c/hwnorm1c.txt
+HWNORM1C=../hwnorm1c.txt
 OUTDIR=output/all
 python whitelist.py $HWNORM1C graylist.txt $OUTDIR
