d798537aa42f3702da3114692d8b6223173c387e
funderburkjim <funderburkjim@gmail.com>
2016-09-13 15:25:09 -0400

    words appearing only in selected pairs of dictionaries

---
+++ ejf/hwnorm1c/whitelist/redo_pairs.sh
@@ -0,0 +1,7 @@
+# Per request of Gasuns, Dhaval
+# Generate lists of words appearing in certain pairs of dictionaries
+python filterdict.py output/all/whitelist0.txt output/all/wil_yat.txt wil yat
+python filterdict.py output/all/whitelist0.txt output/all/skd_vcp.txt skd vcp
+python filterdict.py output/all/whitelist0.txt output/all/pw_pwg.txt pw pwg
+python filterdict.py output/all/whitelist0.txt output/all/mw72_mw.txt mw72 mw
+
