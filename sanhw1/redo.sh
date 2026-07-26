d52f08df438602a9cfaedd8e1598aff5d3153801
Dr. Dhaval Patel <drdhaval2785@gmail.com>
2026-07-26 19:34:44 +0530

    regen after BEN subheadword addition

--- sanhw1/redo.sh
+++ sanhw1/redo.sh
@@ -1,10 +1,10 @@
 echo "1a) remake sanhw1.txt"
-python sanhw1.py sanhw1.txt
+python3 sanhw1.py sanhw1.txt
 echo "1b) remake hwnorm1c.txt"
-python hwnorm1c.py sanhw1.txt hwnorm1c.txt
+python3 hwnorm1c.py sanhw1.txt hwnorm1c.txt
 #
 echo "2) hwnorm1c_sql_input.txt"
-python make_hwnorm1c_sql_input.py hwnorm1c.txt hwnorm1c_sql_input.txt
+python3 make_hwnorm1c_sql_input.py hwnorm1c.txt hwnorm1c_sql_input.txt
 echo "3) hwnorm1c.sqlite"
 rm hwnorm1c.sqlite
 sqlite3 hwnorm1c.sqlite < hwnorm1c.sql
